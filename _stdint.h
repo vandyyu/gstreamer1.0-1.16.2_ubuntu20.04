0a68139de63d1a48ea7bb443d11ba7398d43c178
vandyyu <vandyyu@zhaoxin.com>
2020-10-20 10:40:29 +0800

    init project.

---
+++ _stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GSTREAMER__STDINT_H
+#define _GSTREAMER__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gstreamer 1.16.2"
+/* generated using gnu compiler gcc (Ubuntu 9.3.0-10ubuntu2) 9.3.0 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
